dda7452e229c3b3fa348844b87a57ceff4b830b8
LenaKyrylenko <lena.kirilenko13@gmail.com>
2023-05-12 13:32:45 +0300

    Update method Bayes

--- diploma.py
+++ diploma.py
@@ -8,6 +8,12 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from scipy.stats import entropy
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix,r2_score,f1_score
+from sklearn.model_selection import train_test_split,GridSearchCV,cross_val_score
+from imblearn.under_sampling import RandomUnderSampler
+from functools import reduce
 # df.head()
 # df.info()
 
@@ -20,9 +26,9 @@ from scipy.stats import entropy
 
 
 def replace_values(val):
-    if 1 <= val <= 3:
+    if val >= 1 and val <= 3:
         return 1
-    elif 4 <= val <= 7:
+    elif  val >=4 and val <= 7:
         return 0
     else:
         return val
@@ -37,22 +43,21 @@ def clear_data(df):
                        'OTHER_DISEASE', 'CARDIOVASCULAR', 'OBESITY' , 'RENAL_CHRONIC',
                        'TOBACCO',
                        ]
-    # заменить значения "2" на "0" в указанных столбцах
+     # заменить значения "2" на "0" в указанных столбцах
     for col in cols_to_replace:
         df[col].replace(2, 0, inplace=True)
     num_rows = len(df)
-    # print('КОЛИЧЕСТВО ДО ', num_rows)
     #удаление рядков с 
-    # df = df.loc[df['DATE_DIED'] == '9999-99-99']
+    
+    df = df.loc[df['DATE_DIED'] == '9999-99-99']
     # num_rows = len(df)
-    # print('КОЛИЧЕСТВО ПОСЛЕ', num_rows)
+
     #удаление столбцов о дате сметри и смерти
-    # df = df.drop(columns=["DATE_DIED", "PREGNANT", "INTUBED", "ICU"])
+    df = df.drop(columns=["DATE_DIED", "PREGNANT", "INTUBED", "ICU"])
     for col in df.columns:
         condition = df[(df[col] == 97) | (df[col] == 98) | (df[col] == 99)]
         df.drop(condition.index,inplace=True)
     df.to_csv('updated_table.csv', index=False)
-
 
 # def shannon_entropy(labels):
 #     """Вычисляет информационную энтропию для массива меток."""
@@ -171,38 +176,6 @@ def feature_information_gain(X, y, feature):
     info_gain = entropy(X) - feature_entropy
     return info_gain
 
-
-# target = 'Длина почки'
-# features = ['Age', 'Ширина почки', 'Толщина', 'Поренхима','Ускорение']
-# функция для расчета энтропии
-def entropy(target_col):
-    elements, counts = np.unique(target_col, return_counts = True)
-    entropy = np.sum([(-counts[i]/np.sum(counts)) * np.log2(counts[i]/np.sum(counts)) for i in range(len(elements))])
-    return entropy
-
-# функция для расчета информативности признака
-def information_gain(data, feature, target):
-    total_entropy = entropy(data[target])
-    values, counts = np.unique(data[feature], return_counts=True)
-    weighted_entropy = np.sum([(counts[i]/np.sum(counts)) * entropy(data.where(data[feature]==values[i]).dropna()[target]) for i in range(len(values))])
-    info_gain = total_entropy - weighted_entropy
-    return info_gain
-
-# pd.options.mode.chained_assignment = None  # default='warn'
-# df = pd.read_excel('data1.xlsx')
-df = pd.read_csv('updated_table.csv')
-scaler = StandardScaler()
-    
-    # нормализуем данные
-normalized_data = scaler.fit_transform(df)
-column_names = df.columns.tolist()
-    # создаем DataFrame из нормализованных данных
-normalized_df = pd.DataFrame(normalized_data, columns=column_names)
-
-# clear_data(df)
-# for feature in features:
-#     info_gain = feature_information_gain(df[feature], df[target], feature)
-#     print(f'{feature}: {info_gain}')
 def check_shennon():    
         # список всех признаков, за исключением целевой переменной
     features = list(df.columns[:-1])
@@ -231,16 +204,63 @@ def check_shennon():
     age_entropy = shannon_entropy(df, 'Ускорение')
     print("Entropy of Ускорение: ", age_entropy)
     
+# target = 'Длина почки'
+# features = ['Age', 'Ширина почки', 'Толщина', 'Поренхима','Ускорение']
+# функция для расчета энтропии
+def entropy(target_col):
+    elements, counts = np.unique(target_col, return_counts = True)
+    entropy = np.sum([(-counts[i]/np.sum(counts)) * np.log2(counts[i]/np.sum(counts)) for i in range(len(elements))])
+    return entropy
+
+# функция для расчета информативности признака
+def information_gain(data, feature, target):
+    total_entropy = entropy(data[target])
+    values, counts = np.unique(data[feature], return_counts=True)
+    weighted_entropy = np.sum([(counts[i]/np.sum(counts)) * entropy(data.where(data[feature]==values[i]).dropna()[target]) for i in range(len(values))])
+    info_gain = total_entropy - weighted_entropy
+    return info_gain
+
+pd.options.mode.chained_assignment = None  # default='warn'
+# df = pd.read_excel('data1.xlsx')
+df = pd.read_csv('updated_table.csv')
+
+# clear_data(df)
+for col in df.columns:
+        condition = df[(df[col] == 97) | (df[col] == 98) | (df[col] == 99)]
+        df.drop(condition.index,inplace=True)
+        
+for i in df.columns:
+    print(df[i].value_counts())
+    print("\n")
+scaler = StandardScaler()
+    
+    # нормализуем данные
+normalized_data = scaler.fit_transform(df)
+column_names = df.columns.tolist()
+    # создаем DataFrame из нормализованных данных
+normalized_df = pd.DataFrame(normalized_data, columns=column_names)
+
+# clear_data(df)
+# for feature in features:
+#     info_gain = feature_information_gain(df[feature], df[target], feature)
+#     print(f'{feature}: {info_gain}')
+
 # clear_data(df)
 # method_shennon()
-method_shennon2(normalized_df)
+# method_shennon2(normalized_df)
 print()
-# num_rows = len(df)
+num_rows = len(df)
 # print('КОЛИЧЕСТВО ', num_rows)
-pC1 = count_ones('CLASIFFICATION_FINAL')
-pC2 = len(df) - count_ones('CLASIFFICATION_FINAL')
+print("num_rows", num_rows)
+count_pc1 = count_ones('CLASIFFICATION_FINAL')
+count_pc2  = (len(df) - count_ones('CLASIFFICATION_FINAL'))
+pC1 =count_ones('CLASIFFICATION_FINAL') / num_rows
+pC2 = (len(df) - count_ones('CLASIFFICATION_FINAL'))/num_rows
+
 print("хворі = ",pC1)
 print("здорові = ",pC2)
+# df = df.drop(columns=["USMER"])
+
 
 def check_age(age,p_covid, pC1):
     if age >= 20 and age < 30:
@@ -258,39 +278,96 @@ def check_age(age,p_covid, pC1):
     
 def p_ones_age(age_from, age_up_to,  p_covid,pc1):
     count = 0
-    return ((df['AGE'] >= age_from) &(df['AGE'] <= age_up_to) & (df['CLASIFFICATION_FINAL'] == pc1)).sum() / p_covid
+    return ((df['AGE'] >= age_from) &(df['AGE'] <= age_up_to) & (df['CLASIFFICATION_FINAL'] == pc1))
        
 def p_ones(column_name, p_covid):
     return ((df[column_name] == 1) & (df['CLASIFFICATION_FINAL'] == 1)).sum() / p_covid
 
+def normalize_resultPC1(resC1, resC2 ):
+    return resC1/(resC1+resC2)
 
-def calc(usmer = 1, medical_unit = 2, sex = 1, patient_type = 1,
-          intubed = 1, pneumonia = 1, age = 30, diabetes = 0, copd = 0, astma = 0, tobacco = 0,pc1 = 1,  p_covid=pC1):
-    # res_usmer =  ((df['USMER'] == usmer) & (df['CLASIFFICATION_FINAL'] == pc1)).sum() / p_covid
+def normalize_resultPC2(resC1, resC2 ):
+    return resC2/(resC1+resC2)
+
+
+def calc(pC1 = pC1, usmer = 1, medical_unit = 2, sex = 1, patient_type = 1, medical = 3, other = 0,  obesity=1,
+          intubed = 1, pneumonia = 1, age = 50, diabetes = 1, copd = 1, astma = 1, tobacco = 0,pc1 = 1,  p_covid=count_pc1):
+    res_usmer =  ((df['USMER'] == usmer) & (df['CLASIFFICATION_FINAL'] == pc1)).sum() / p_covid
+    res_patient_type =  ((df['PATIENT_TYPE'] == patient_type) & (df['CLASIFFICATION_FINAL'] == pc1)).sum() / p_covid
+    res_medical = ((df['MEDICAL_UNIT'] == medical) & (df['CLASIFFICATION_FINAL'] == pc1)).sum() / p_covid
     res_sex =  (((df['SEX'] == sex) & (df['CLASIFFICATION_FINAL'] == pc1)).sum()) / p_covid
-    # res_inubed = ((df['INTUBED'] == intubed) & (df['CLASIFFICATION_FINAL'] == pc1)).sum() / p_covid
     res_pneumonia = ((df['PNEUMONIA'] == pneumonia) & (df['CLASIFFICATION_FINAL'] == pc1)).sum() / p_covid
     res_age = check_age(age,p_covid, pc1)
     res_diabetes = ((df['DIABETES'] == diabetes) & (df['CLASIFFICATION_FINAL'] == pc1)).sum() / p_covid
     res_copd = ((df['COPD'] == copd) & (df['CLASIFFICATION_FINAL'] ==pc1)).sum() / p_covid
     res_astma = ((df['ASTHMA'] == astma) & (df['CLASIFFICATION_FINAL'] == pc1)).sum() / p_covid
     res_tobacco = ((df['TOBACCO'] == tobacco) & (df['CLASIFFICATION_FINAL'] ==pc1)).sum() / p_covid
-    resultPC1 =  res_sex  *res_pneumonia*res_age * res_diabetes * res_copd * res_astma * res_tobacco
+    res_obesity = ((df['OBESITY'] == obesity) & (df['CLASIFFICATION_FINAL'] ==pc1)).sum() / p_covid
+    # res = res_sex * res_medical * pC1
     
+    resultPC1 =res_sex  * res_obesity * res_medical * res_pneumonia * res_age * res_diabetes * res_copd * res_astma * res_tobacco* pC1
+    
+    print("res_medical = ", res_medical)
     print("res_sex = ", res_sex)
-    # print("res_inubed = ", res_inubed)
-    print("res_pneumonia = ", res_pneumonia)
-    print("res_age = ", res_age)
-    print("res_diabetes = ", res_diabetes)
-    print("res_copd = ", res_copd)
-    print("res_astma = ", res_copd)
-    print("res_tobacco = ", res_copd)
+    # print("res = ", res)
+    # print("res_pneumonia = ", res_pneumonia)
+    # print("res_age = ", res_age)
+    # print("res_diabetes = ", res_diabetes)
+    # print("res_copd = ", res_copd)
+    # print("res_astma = ", res_copd)
+    # print("res_tobacco = ", res_copd)
     
     return  resultPC1
 
 print("Вхідні дані: \nsex = 1,intubed = 1, pneumonia = 1, age = 30, diabetes = 0, copd = 0, astma = 0, tobacco = 0")
-print("імовірність захворіти = ",calc(pc1 = 1))
-print("імовірність здоровий = ",calc(p_covid=pC2, pc1 = 0))
+# resC1 = calc(pc1 = 1)
+# resC2 = calc(pC1= pC2, p_covid=count_pc2, pc1 = 0)
+
+# print("імовірність захворіти = ", normalize_resultPC1(resC1, resC2))
+# print("імовірність здоровий = ",normalize_resultPC2(resC1, resC2))
+
+print("medical unit від 1 до 13")
+print("usmer - чи лікувався в медичних закладах")
+print("patient_type - 1 - лікується вдома, 0 госпіталізован")
+print("INMSUPR - ослаблення імунітету, 1 - так, 0 - ні")
+print("HIPERTENSION - підвищенний кров'яний тиск 1 - так, 0 - ні")
+print("RENAL_CHRONIC - хронічна хвороба нирок")
+
+user_input = pd.read_csv('user1.csv')
+columns_to_check = ['USMER',  'MEDICAL_UNIT', 'SEX','PATIENT_TYPE', 'PNEUMONIA','AGE', 'DIABETES', 
+                    'COPD', 'ASTHMA','INMSUPR', 'HIPERTENSION','OTHER_DISEASE',
+                    'CARDIOVASCULAR', 'OBESITY','RENAL_CHRONIC',
+                    'TOBACCO']
+# for i in user_input.columns:
+#     print(user_input[i].value_counts())
+#     print("\n")
+    
+# print(df.columns)
+# print(user_input.columns)
+
+def probility_sick(pC1= pC1, user_input = user_input,pc1 = 1, p_covid = count_pc1):
+    result_dict = {}
+    for column, value in user_input.items():
+        # print("column ", column)
+        if column == 'AGE':
+            age = int(user_input[column])
+            relevant_data = check_age(age,p_covid, pc1)
+        else:
+            relevant_data = (df[column] == int(value)) & (df['CLASIFFICATION_FINAL'] == pc1)
+        likelihood = relevant_data.sum() / p_covid
+        result_dict[column] = likelihood
+    result_dict.popitem() # видаляємо останній запис
+    resultPC1 = pC1 * reduce(lambda x, y: x * y, result_dict.values())
+    print()
+    print(" результат до нормалізації =  ", resultPC1)
+    print()
+    print("result dict ", result_dict)
+    return resultPC1
+resC1 = probility_sick(pc1 = 1)
+resC2 = probility_sick(pC1= pC2, p_covid=count_pc2, pc1 = 0)
+print()
+print("імовірність захворіти = ", normalize_resultPC1(resC1, resC2))
+print("імовірність здоровий = ",normalize_resultPC2(resC1, resC2))
 
     # features = df.drop('CLASIFFICATION_FINAL', axis=1)
     # labels = df['CLASIFFICATION_FINAL']
@@ -350,7 +427,7 @@ def correlation2(df):
             corr = column_data.corr(df[corr_column])
             print(f"Корреляция между столбцами '{column_name}' и '{corr_column}': {corr}")
     
-correlation2(df)
+# correlation2(df)
 # print("Кореляція")
 # correlation2()
 
